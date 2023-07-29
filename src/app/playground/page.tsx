93a5af598455dba7a0b0e1a12d273e9d263712a5
sungpah <sungpah@gmail.com>
2023-07-29 16:55:28 +0900

    UPdate

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -5,14 +5,22 @@ import { useState, useEffect } from 'react'
 const PlaygroundPage = () => {
   const [prompt, setPrompt] = useState("")
 
-  const [mode, setMode] = useState("initial")
+  const [mode, setMode] = useState("conversation")
   const [arr, setArr] = useState([
+    {
+      type: "prompt",
+      prompt: "nice"
+    },
+    {
+      type: "response",
+      text: 'res..'
+    }
   ])
 
   const submit = async () => {
     setMode('conversation')
 
-    setArr(prev => ([...prev, {type: "prompt", prompt: prompt} ]))
+    setArr(prev => ([...prev, { type: "prompt", prompt: prompt }]))
 
     setPrompt("")
 
@@ -26,20 +34,18 @@ const PlaygroundPage = () => {
       });
 
       const { message } = await apiResponse.json();
-      setArr(prev => ([...prev, {type: "result", text: message} ]))
+      setArr(prev => ([...prev, { type: "result", text: message }]))
     } catch (error) {
       console.error('Error sending message:', error);
     }
-
   }
-
 
   return <div className="flex bg-gray h-screen">
     <div className="w-[260px] bg-red-300">
       list
     </div>
     <div className="flex-1 ">
-      <div style={{ minWidth: 600, maxWidth: 800, margin: 'auto' }} className="bg-black h-full relative">
+      <div style={{}} className="bg-black h-full relative">
 
         {mode === "initial" &&
           <div className="flex flex-col pt-4" style={{ alignItems: 'center', }}>
@@ -65,37 +71,54 @@ const PlaygroundPage = () => {
         }
 
         {mode === "conversation" &&
-        <div className="flex flex-col bg-red-300">
-          {
-            arr.map((obj, index) => {
-              if (obj.type === "prompt") {
-                return <div className="bg-black-300 p-4" key={index}>
-                  {obj.prompt}
-              </div>
-
-              }else {
-                return <div className="bg-gray-800 p-4" key={index}>
-                  {obj.text}
-              </div>
-              }
-            })
-          }
-        </div>
+          <div className="flex flex-col">
+            {
+              arr.map((obj, index) => {
+                if (obj.type === "prompt") {
+                  return <div className="bg-gray-600 p-4  " key={index} >
+                    <div style={{ minWidth: 600, maxWidth: 800, margin: 'auto' }} className="relative pb-4">
+                      {obj.prompt}
+                      <div className="absolute left-[-40px]  top-0 w-[32px] h-[32px] 
+                      flex items-center justify-center
+                    rounded-md bg-purple-500"
+                      >S</div>
+                    </div>
+                  </div>
+                } else {
+                  return <div className="bg-gray-800 p-4" key={index}>
+                    <div style={{ minWidth: 600, maxWidth: 800, margin: 'auto' }} className="relative pb-4">
+                    {obj.text}
+                    <div className="absolute left-[-40px]  top-0 w-[32px] h-[32px] 
+                      flex items-center justify-center
+                    rounded-md bg-green-500"
+                      >G</div>
+                    </div>
+                  </div>
+                }
+              })
+            }
+          </div>
         }
 
-        <div className="flex absolute left-0 bottom-[20px] w-full p-4 rounded-lg"
-          style={{ border: '1px solid gray' }}>
-          <textarea
-            value={prompt}
-            onChange={e => setPrompt(e.target.value)}
-            style={{ height: 30 }}
-            placeholder="type here"
-            className="resize-none bg-transparent w-full   focus:outline-none" ></textarea>
+        <div className="absolute left-0 bottom-[20px] w-full">
+          <div className="flex p-4 rounded-lg relative"
+            style={{ border: '1px solid gray', maxWidth:800, margin:'auto' }}>
+            <textarea
+              value={prompt}
+              onChange={e => setPrompt(e.target.value)}
+              style={{ height: 30 }}
+              placeholder="type here"
+              className="resize-none bg-transparent w-full   focus:outline-none" ></textarea>
 
-          <button
-            onClick={submit}
-            className="absolute right-[16px] rounded-md top-[12px] w-[32px] h-[32px]"
-            style={{ backgroundColor: "#28b496" }}>s</button>
+            <button
+              onClick={submit}
+              className={`absolute right-[16px] rounded-md top-[12px] w-[32px] h-[32px]
+              transition-all ease-in-out duration-500
+              ${prompt.trim() === '' ? 'bg-red-500' : 'bg-blue-500 w-[34px]'
+                }
+              `}
+            >s</button>
+          </div>
         </div>
       </div>
     </div>
